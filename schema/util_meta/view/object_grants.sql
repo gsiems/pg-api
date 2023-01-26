6a8df27312dce971a5999a9521c4754003dad10b
Gregory Siems <gsiems@users.noreply.github.com>
2023-01-26 11:31:31 -0600

    Added column privileges. Refactored duplicated portions.

--- schema/util_meta/view/object_grants.sql
+++ schema/util_meta/view/object_grants.sql
@@ -8,33 +8,19 @@ WITH rol AS (
     SELECT 0::oid AS oid,
             'public'::text
 ),
-namespace_base AS (
-    SELECT schemas.schema_oid,
-            schemas.schema_name::text AS object_schema,
-            n.oid,
-            n.nspname::text AS object_name,
+schemas AS ( -- Schemas
+    SELECT oid AS schema_oid,
+            n.nspname::text AS schema_name,
             n.nspowner AS owner_oid,
             'schema'::text AS object_type,
             coalesce ( n.nspacl, acldefault ( 'n'::"char", n.nspowner ) ) AS acl
-        FROM pg_namespace n
-        JOIN util_meta.schemas
-            ON ( schemas.schema_oid = n.oid )
+        FROM pg_catalog.pg_namespace n
+        WHERE n.nspname !~ '^pg_'
+            AND n.nspname <> 'information_schema'
 ),
-namespaces AS (
-    SELECT object_schema,
-            oid,
-            object_name,
-            object_type,
-            owner_oid,
-            ( aclexplode ( acl ) ).grantor AS grantor_oid,
-            ( aclexplode ( acl ) ).grantee AS grantee_oid,
-            ( aclexplode ( acl ) ).privilege_type AS privilege_type,
-            ( aclexplode ( acl ) ).is_grantable AS is_grantable
-        FROM namespace_base
-),
-class_base AS (
+classes AS ( -- Tables, views, etc.
     SELECT schemas.schema_oid,
-            schemas.schema_name::text AS object_schema,
+            schemas.schema_name AS object_schema,
             c.oid,
             c.relname::text AS object_name,
             c.relowner AS owner_oid,
@@ -57,25 +43,26 @@ class_base AS (
                 ELSE coalesce ( c.relacl, acldefault ( 'r'::"char", c.relowner ) )
                 END AS acl
         FROM pg_class c
-        JOIN util_meta.schemas
+        JOIN schemas
             ON ( schemas.schema_oid = c.relnamespace )
         WHERE c.relkind IN ( 'r', 'v', 'm', 'S', 'f', 'p' )
 ),
-classes AS (
-    SELECT object_schema,
-            oid,
-            object_name,
-            object_type,
-            owner_oid,
-            ( aclexplode ( acl ) ).grantor AS grantor_oid,
-            ( aclexplode ( acl ) ).grantee AS grantee_oid,
-            ( aclexplode ( acl ) ).privilege_type AS privilege_type,
-            ( aclexplode ( acl ) ).is_grantable AS is_grantable
-        FROM class_base
+cols AS ( -- Columns
+    SELECT c.object_schema,
+            null::integer AS oid,
+            c.object_name || '.' || a.attname::text AS object_name,
+            'column' AS object_type,
+            c.owner_oid,
+            coalesce ( a.attacl, acldefault ( 'c'::"char", c.owner_oid ) ) AS acl
+        FROM pg_attribute a
+        JOIN classes c
+            ON ( a.attrelid = c.oid )
+        WHERE a.attnum > 0
+            AND NOT a.attisdropped
 ),
-proc_base AS (
+procs AS ( -- Procedures and functions
     SELECT schemas.schema_oid,
-            schemas.schema_name::text AS object_schema,
+            schemas.schema_name AS object_schema,
             p.oid,
             p.proname::text AS object_name,
             p.proowner AS owner_oid,
@@ -85,27 +72,15 @@ proc_base AS (
                 WHEN 'p' THEN 'procedure'
                 ELSE 'function'
                 END AS object_type,
+            pg_catalog.pg_get_function_arguments ( p.oid ) AS calling_arguments,
             coalesce ( p.proacl, acldefault ( 'f'::"char", p.proowner ) ) AS acl
         FROM pg_proc p
-        JOIN util_meta.schemas
+        JOIN schemas
             ON ( schemas.schema_oid = p.pronamespace )
 ),
-procs AS (
-    SELECT object_schema,
-            oid,
-            object_name,
-            object_type,
-            owner_oid,
-            pg_catalog.pg_get_function_arguments ( oid ) AS calling_arguments,
-            ( aclexplode ( acl ) ).grantor AS grantor_oid,
-            ( aclexplode ( acl ) ).grantee AS grantee_oid,
-            ( aclexplode ( acl ) ).privilege_type AS privilege_type,
-            ( aclexplode ( acl ) ).is_grantable AS is_grantable
-        FROM proc_base
-),
-udt_base AS (
+udts AS ( -- User defined types
     SELECT schemas.schema_oid,
-            schemas.schema_name::text AS object_schema,
+            schemas.schema_name AS object_schema,
             t.oid,
             t.typname::text AS object_name,
             t.typowner AS owner_oid,
@@ -121,7 +96,7 @@ udt_base AS (
                 END AS object_type,
             coalesce ( t.typacl, acldefault ( 'T'::"char", t.typowner ) ) AS acl
         FROM pg_type t
-        JOIN util_meta.schemas
+        JOIN schemas
             ON ( schemas.schema_oid = t.typnamespace )
         WHERE ( t.typrelid = 0
                 OR ( SELECT c.relkind = 'c'
@@ -136,19 +111,7 @@ udt_base AS (
         --AND NOT ( t.typtype = 'c'
         --    AND n.nspname = 'pg_catalog' )
 ),
-udts AS (
-    SELECT object_schema,
-            oid,
-            object_name,
-            object_type,
-            owner_oid,
-            ( aclexplode ( acl ) ).grantor AS grantor_oid,
-            ( aclexplode ( acl ) ).grantee AS grantee_oid,
-            ( aclexplode ( acl ) ).privilege_type AS privilege_type,
-            ( aclexplode ( acl ) ).is_grantable AS is_grantable
-        FROM udt_base
-),
-fdw_base AS (
+fdws AS ( -- Foreign data wrappers
     SELECT null::oid AS schema_oid,
             null::text AS object_schema,
             p.oid,
@@ -158,19 +121,7 @@ fdw_base AS (
             coalesce ( p.fdwacl, acldefault ( 'F'::"char", p.fdwowner ) ) AS acl
         FROM pg_foreign_data_wrapper p
 ),
-fdws AS (
-    SELECT object_schema,
-            oid,
-            object_name,
-            object_type,
-            owner_oid,
-            ( aclexplode ( acl ) ).grantor AS grantor_oid,
-            ( aclexplode ( acl ) ).grantee AS grantee_oid,
-            ( aclexplode ( acl ) ).privilege_type AS privilege_type,
-            ( aclexplode ( acl ) ).is_grantable AS is_grantable
-        FROM fdw_base
-),
-fsrv_base AS (
+fsrvs AS ( -- Foreign servers
     SELECT null::oid AS schema_oid,
             null::text AS object_schema,
             p.oid,
@@ -180,138 +131,101 @@ fsrv_base AS (
             coalesce ( p.srvacl, acldefault ( 'S'::"char", p.srvowner ) ) AS acl
         FROM pg_foreign_server p
 ),
-fsrvs AS (
-    SELECT object_schema,
-            oid,
-            object_name,
+all_objects AS (
+    SELECT schema_name AS object_schema,
             object_type,
+            schema_name AS object_name,
+            null::text AS calling_arguments,
+            owner_oid,
+            acl
+        FROM schemas
+    UNION
+    SELECT object_schema,
+            object_type,
+            object_name,
+            null::text AS calling_arguments,
+            owner_oid,
+            acl
+        FROM classes
+    UNION
+    SELECT object_schema,
+            object_type,
+            object_name,
+            null::text AS calling_arguments,
+            owner_oid,
+            acl
+        FROM cols
+    UNION
+    SELECT object_schema,
+            object_type,
+            object_name,
+            calling_arguments,
+            owner_oid,
+            acl
+        FROM procs
+    UNION
+    SELECT object_schema,
+            object_type,
+            object_name,
+            null::text AS calling_arguments,
+            owner_oid,
+            acl
+        FROM udts
+    UNION
+    SELECT object_schema,
+            object_type,
+            object_name,
+            null::text AS calling_arguments,
+            owner_oid,
+            acl
+        FROM fdws
+    UNION
+    SELECT object_schema,
+            object_type,
+            object_name,
+            null::text AS calling_arguments,
+            owner_oid,
+            acl
+        FROM fsrvs
+),
+acl_base AS (
+    SELECT object_schema,
+            object_type,
+            object_name,
+            calling_arguments,
             owner_oid,
             ( aclexplode ( acl ) ).grantor AS grantor_oid,
             ( aclexplode ( acl ) ).grantee AS grantee_oid,
             ( aclexplode ( acl ) ).privilege_type AS privilege_type,
             ( aclexplode ( acl ) ).is_grantable AS is_grantable
-        FROM fsrv_base
+        FROM all_objects
 )
--- schema privileges
-SELECT namespaces.object_schema,
-        namespaces.object_type,
-        namespaces.object_name,
-        null::text AS calling_arguments,
+SELECT acl_base.object_schema,
+        acl_base.object_type,
+        acl_base.object_name,
+        acl_base.calling_arguments,
         owner.role_name AS object_owner,
         grantor.role_name AS grantor,
         grantee.role_name AS grantee,
-        namespaces.privilege_type,
-        namespaces.is_grantable
-    FROM namespaces
+        acl_base.privilege_type,
+        acl_base.is_grantable
+    FROM acl_base
     JOIN rol owner
-        ON ( owner.oid = namespaces.owner_oid )
+        ON ( owner.oid = acl_base.owner_oid )
     JOIN rol grantor
-        ON ( grantor.oid = namespaces.grantor_oid )
+        ON ( grantor.oid = acl_base.grantor_oid )
     JOIN rol grantee
-        ON ( grantee.oid = namespaces.grantee_oid )
-    WHERE grantor.oid <> grantee.oid
-UNION
--- class privileges
-SELECT classes.object_schema,
-        classes.object_type,
-        classes.object_name,
-        null::text AS calling_arguments,
-        owner.role_name AS object_owner,
-        grantor.role_name AS grantor,
-        grantee.role_name AS grantee,
-        classes.privilege_type,
-        classes.is_grantable
-    FROM classes
-    JOIN rol owner
-        ON ( owner.oid = classes.owner_oid )
-    JOIN rol grantor
-        ON ( grantor.oid = classes.grantor_oid )
-    JOIN rol grantee
-        ON ( grantee.oid = classes.grantee_oid )
-    WHERE grantor.oid <> grantee.oid
-UNION
--- routine_privileges
-SELECT procs.object_schema,
-        procs.object_type,
-        procs.object_name,
-        procs.calling_arguments,
-        owner.role_name AS object_owner,
-        grantor.role_name AS grantor,
-        grantee.role_name AS grantee,
-        procs.privilege_type,
-        procs.is_grantable
-    FROM procs
-    JOIN rol owner
-        ON ( owner.oid = procs.owner_oid )
-    JOIN rol grantor
-        ON ( grantor.oid = procs.grantor_oid )
-    JOIN rol grantee
-        ON ( grantee.oid = procs.grantee_oid )
-    WHERE grantor.oid <> grantee.oid
-UNION
--- udt_privileges
-SELECT udts.object_schema,
-        udts.object_type,
-        udts.object_name,
-        null::text AS calling_arguments,
-        owner.role_name AS object_owner,
-        grantor.role_name AS grantor,
-        grantee.role_name AS grantee,
-        udts.privilege_type,
-        udts.is_grantable
-    FROM udts
-    JOIN rol owner
-        ON ( owner.oid = udts.owner_oid )
-    JOIN rol grantor
-        ON ( grantor.oid = udts.grantor_oid )
-    JOIN rol grantee
-        ON ( grantee.oid = udts.grantee_oid )
-    WHERE grantor.oid <> grantee.oid
-UNION
--- fdw privileges
-SELECT fdws.object_schema,
-        fdws.object_type,
-        fdws.object_name,
-        null::text AS calling_arguments,
-        owner.role_name AS object_owner,
-        grantor.role_name AS grantor,
-        grantee.role_name AS grantee,
-        fdws.privilege_type,
-        fdws.is_grantable
-    FROM fdws
-    JOIN rol owner
-        ON ( owner.oid = fdws.owner_oid )
-    JOIN rol grantor
-        ON ( grantor.oid = fdws.grantor_oid )
-    JOIN rol grantee
-        ON ( grantee.oid = fdws.grantee_oid )
-    WHERE grantor.oid <> grantee.oid
-UNION
--- foreign servers
-SELECT fsrvs.object_schema,
-        fsrvs.object_type,
-        fsrvs.object_name,
-        null::text AS calling_arguments,
-        owner.role_name AS object_owner,
-        grantor.role_name AS grantor,
-        grantee.role_name AS grantee,
-        fsrvs.privilege_type,
-        fsrvs.is_grantable
-    FROM fsrvs
-    JOIN rol owner
-        ON ( owner.oid = fsrvs.owner_oid )
-    JOIN rol grantor
-        ON ( grantor.oid = fsrvs.grantor_oid )
-    JOIN rol grantee
-        ON ( grantee.oid = fsrvs.grantee_oid )
-    WHERE grantor.oid <> grantee.oid ;
+        ON ( grantee.oid = acl_base.grantee_oid )
+    WHERE acl_base.grantor_oid <> acl_base.grantee_oid ;
 
 COMMENT ON VIEW util_meta.object_grants IS 'Privilege grants for application database objects' ;
 
 COMMENT ON COLUMN util_meta.object_grants.object_schema IS 'The name of the schema that contains the object.' ;
 COMMENT ON COLUMN util_meta.object_grants.object_name IS 'The name of the database object.' ;
 COMMENT ON COLUMN util_meta.object_grants.object_type IS 'The type of the database object.' ;
-COMMENT ON COLUMN util_meta.object_grants.grantor IS 'The user/role that granted the privilege' ;
-COMMENT ON COLUMN util_meta.object_grants.grantee IS 'The user/role that the privilege was granted to' ;
-COMMENT ON COLUMN util_meta.object_grants.privilege_type IS 'The type of privilege' ;
-COMMENT ON COLUMN util_meta.object_grants.is_grantable IS 'Indicates if the privilege is grantable' ;
+COMMENT ON COLUMN util_meta.object_grants.calling_arguments IS '(function and procedure only) The list of calling arguments to the function or procedure.' ;
+COMMENT ON COLUMN util_meta.object_grants.object_owner IS 'The owner of the database object.' ;
+COMMENT ON COLUMN util_meta.object_grants.grantor IS 'The user/role that granted the privilege.' ;
+COMMENT ON COLUMN util_meta.object_grants.grantee IS 'The user/role that the privilege was granted to.' ;
+COMMENT ON COLUMN util_meta.object_grants.privilege_type IS 'The type of privilege.' ;
+COMMENT ON COLUMN util_meta.object_grants.is_grantable IS 'Indicates if the privilege may be granted to others.' ;
