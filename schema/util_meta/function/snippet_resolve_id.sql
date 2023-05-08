7e1e7a266a6db7c76d102c3709742d905b34cab2
Gregory Siems <gsiems@gmail.com>
2023-05-07 19:54:00 -0500

    Add indents parameter

--- schema/util_meta/function/snippet_resolve_id.sql
+++ schema/util_meta/function/snippet_resolve_id.sql
@@ -1,4 +1,5 @@
 CREATE OR REPLACE FUNCTION util_meta.snippet_resolve_id (
+    a_indents integer default null,
     a_id_param text default null,
     a_function_schema text default null,
     a_function_name text default null,
@@ -13,25 +14,29 @@ Function snippet_resolve_id generates the pl/pg-sql code snippet for calling a r
 
 | Parameter                      | In/Out | Datatype   | Remarks                                            |
 | ------------------------------ | ------ | ---------- | -------------------------------------------------- |
+| a_indents                      | in     | integer    | The number of indentations to prepend to each line of the code snippet (default 0) |
 | a_id_param                     | in     | text       | The (name of the) parameter to set the resolved ID to  |
 | a_function_schema              | in     | text       | The (name of the) schema to find the function in   |
 | a_function_name                | in     | text       | The (name of the) kind of ID resolution function   |
-| a_resolve_id_params            | in     | text       | The list of parameters for the ID resolution function |
+| a_resolve_id_params            | in     | text[]     | The list of parameters for the ID resolution function |
 
 */
 DECLARE
 
     l_resolve_id_params text[] ;
+    l_indents integer ;
 
 BEGIN
+
+    l_indents := coalesce ( a_indents, 0 ) ;
 
     FOR idx IN 1..array_length ( a_resolve_id_params, 1 ) LOOP
         l_resolve_id_params := array_append ( l_resolve_id_params, concat_ws ( ' ', a_resolve_id_params[idx], '=>', a_resolve_id_params[idx] ) ) ;
     END LOOP ;
 
     RETURN concat_ws ( util_meta.new_line (),
-        util_meta.indent (1) || concat_ws ( ' ', a_id_param, ':=', a_function_schema || '.' || a_function_name, '(' ),
-        util_meta.indent (2) || array_to_string ( l_resolve_id_params, ',' || util_meta.new_line () || util_meta.indent (2) ) || ' ) ;' ) ;
+        util_meta.indent (l_indents + 1) || concat_ws ( ' ', a_id_param, ':=', a_function_schema || '.' || a_function_name, '(' ),
+        util_meta.indent (l_indents + 2) || array_to_string ( l_resolve_id_params, ',' || util_meta.new_line () || util_meta.indent (l_indents + 2) ) || ' ) ;' ) ;
 
 END ;
 $$ ;
