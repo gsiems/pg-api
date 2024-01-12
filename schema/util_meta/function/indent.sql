4a212d28bb32350fdeb74ad85c10eedd118a4207
Gregory Siems <gsiems@gmail.com>
2024-01-11 23:35:46 -0600

    Support changing the default indentation

--- schema/util_meta/function/indent.sql
+++ schema/util_meta/function/indent.sql
@@ -5,14 +5,23 @@ LANGUAGE sql
 STABLE
 SECURITY DEFINER
 AS $$
+/**
+Function indent returns the specified amount of indentation for start of a line
+
+| Parameter                      | In/Out | Datatype   | Description                                        |
+| ------------------------------ | ------ | ---------- | -------------------------------------------------- |
+| a_count                        | in     | integer    | The number of indents                              |
+
+*/
 
     WITH x AS (
-        SELECT '    ' AS spaces
+        SELECT coalesce ( util_meta.resolve_parameter (
+                    a_name => 'indent_char' ), '    ' ) AS chars
     )
     SELECT CASE
-                WHEN a_count IS NULL THEN spaces
-                WHEN a_count BETWEEN 1 AND 10 THEN repeat ( spaces, a_count )
-                ELSE spaces
+                WHEN a_count IS NULL THEN chars
+                WHEN a_count BETWEEN 1 AND 10 THEN repeat ( chars, a_count )
+                ELSE chars
                 END AS indent
         FROM x ;
 
