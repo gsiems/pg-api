b9197eaf25de96edd1059fa1acafc96c6e7a5433
Gregory Siems <gsiems@gmail.com>
2024-03-17 16:12:53 -0500

    Remove unnecessary cte

--- schema/util_meta/view/schemas.sql
+++ schema/util_meta/view/schemas.sql
@@ -1,20 +1,16 @@
 CREATE OR REPLACE VIEW util_meta.schemas
 AS
-WITH base AS (
-    SELECT n.oid AS schema_oid,
-            n.nspname::text AS schema_name,
-            n.nspowner AS owner_oid,
-            pg_catalog.pg_get_userbyid ( n.nspowner )::text AS owner_name,
-            concat_ws ( '/', 'schema', n.nspname::text ) AS directory_name
-        FROM pg_catalog.pg_namespace n
-        LEFT JOIN pg_catalog.pg_extension px
-            ON ( px.extnamespace = n.oid )
-        WHERE n.nspname !~ '^pg_'
-            AND n.nspname <> 'information_schema'
-            AND px.oid IS NULL
-)
-SELECT *
-    FROM base ;
+SELECT n.oid AS schema_oid,
+        n.nspname::text AS schema_name,
+        n.nspowner AS owner_oid,
+        pg_catalog.pg_get_userbyid ( n.nspowner )::text AS owner_name,
+        concat_ws ( '/', 'schema', n.nspname::text ) AS directory_name
+    FROM pg_catalog.pg_namespace n
+    LEFT JOIN pg_catalog.pg_extension px
+        ON ( px.extnamespace = n.oid )
+    WHERE n.nspname !~ '^pg_'
+        AND n.nspname <> 'information_schema'
+        AND px.oid IS NULL ;
 
 COMMENT ON VIEW util_meta.schemas IS 'View of the application database schemas' ;
 COMMENT ON COLUMN util_meta.schemas.schema_oid IS 'The OID of the schema.' ;
