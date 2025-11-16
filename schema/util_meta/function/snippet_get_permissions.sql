3f2661781773778f2c3050626fbc2f912d2dae24
Gregory Siems <gsiems@gmail.com>
2025-11-16 16:09:53 -0600

    Add search_path. Add upsert. Format using https://github.com/gsiems/sqlfmt

--- schema/util_meta/function/snippet_get_permissions.sql
+++ schema/util_meta/function/snippet_get_permissions.sql
@@ -1,15 +1,16 @@
 CREATE OR REPLACE FUNCTION util_meta.snippet_get_permissions (
-    a_indents integer default null,
-    a_action text default null,
-    a_ddl_schema text default null,
-    a_object_type text default null,
-    a_id_param text default null,
-    a_parent_object_type text default null,
-    a_parent_id_param text default null )
+    a_indents integer DEFAULT NULL,
+    a_action text DEFAULT NULL,
+    a_ddl_schema text DEFAULT NULL,
+    a_object_type text DEFAULT NULL,
+    a_id_param text DEFAULT NULL,
+    a_parent_object_type text DEFAULT NULL,
+    a_parent_id_param text DEFAULT NULL )
 RETURNS text
 LANGUAGE plpgsql
 STABLE
 SECURITY DEFINER
+SET search_path = pg_catalog, util_meta
 AS $$
 /**
 Function snippet_get_permissions generates the pl/pgsql code snippet for calling a getting the users permissions for an action on an object
@@ -46,7 +47,7 @@ BEGIN
 
     l_indents := coalesce ( a_indents, 0 ) ;
 
-    IF substring ( a_object_type for 3 ) = 'rt_' THEN
+    IF substring ( a_object_type FOR 3 ) = 'rt_' THEN
         l_object_type := 'ref_data' ;
         l_id_param := 'null' ;
     ELSE
@@ -55,6 +56,8 @@ BEGIN
 
     IF a_action = 'l_desired_action' THEN
         l_action := a_action ;
+    ELSIF a_action = 'upsert' THEN
+        l_action := 'CASE WHEN ' || a_id_param || ' IS NULL THEN ''insert'' ELSE ''update'' END' ;
     ELSE
         l_action := quote_literal ( a_action ) ;
     END IF ;
@@ -63,25 +66,35 @@ BEGIN
 
         l_id_param := coalesce ( l_id_param, a_id_param, 'null' ) ;
 
-        RETURN concat_ws ( util_meta.new_line (),
-                '',
-                util_meta.indent (l_indents + 1) || 'l_has_permission := ' || a_ddl_schema || '.can_do (',
-                util_meta.indent (l_indents + 2) || 'a_user => a_user,',
-                util_meta.indent (l_indents + 2) || 'a_action => ' || l_action || ',',
-                util_meta.indent (l_indents + 2) || 'a_object_type => ' || quote_literal ( l_object_type ) || ',  -- TODO verify this',
-                util_meta.indent (l_indents + 2) || 'a_id => ' || l_id_param || ' ) ; -- TODO verify this' ) ;
+        RETURN concat_ws (
+            util_meta.new_line (),
+            '',
+            util_meta.indent ( l_indents + 1 ) || 'l_has_permission := ' || a_ddl_schema || '.can_do (',
+            util_meta.indent ( l_indents + 2 ) || 'a_user => a_user,',
+            util_meta.indent ( l_indents + 2 ) || 'a_action => ' || l_action || ',',
+            util_meta.indent ( l_indents + 2 )
+                || 'a_object_type => '
+                || quote_literal ( l_object_type )
+                || ',  -- TODO verify this',
+            util_meta.indent ( l_indents + 2 ) || 'a_id => ' || l_id_param || ' ) ; -- TODO verify this' ) ;
 
     END IF ;
 
-    RETURN concat_ws ( util_meta.new_line (),
-            '',
-            util_meta.indent (l_indents + 1) || 'l_has_permission := ' || a_ddl_schema || '.can_do (',
-            util_meta.indent (l_indents + 2) || 'a_user => a_user,',
-            util_meta.indent (l_indents + 2) || 'a_action => ' || l_action || ',',
-            util_meta.indent (l_indents + 2) || 'a_object_type => ' || quote_literal ( l_object_type ) || ',  -- TODO verify this',
-            util_meta.indent (l_indents + 2) || 'a_id => null,',
-            util_meta.indent (l_indents + 2) || 'a_parent_object_type => ' || quote_literal ( a_parent_object_type ) || ',',
-            util_meta.indent (l_indents + 2) || 'a_parent_object_id => ' || a_parent_id_param || ' ) ;' ) ;
+    RETURN concat_ws (
+        util_meta.new_line (),
+        '',
+        util_meta.indent ( l_indents + 1 ) || 'l_has_permission := ' || a_ddl_schema || '.can_do (',
+        util_meta.indent ( l_indents + 2 ) || 'a_user => a_user,',
+        util_meta.indent ( l_indents + 2 ) || 'a_action => ' || l_action || ',',
+        util_meta.indent ( l_indents + 2 )
+            || 'a_object_type => '
+            || quote_literal ( l_object_type )
+            || ',  -- TODO verify this',
+        util_meta.indent ( l_indents + 2 ) || 'a_id => null,',
+        util_meta.indent ( l_indents + 2 )
+            || 'a_parent_object_type => '
+            || quote_literal ( a_parent_object_type ) || ',',
+        util_meta.indent ( l_indents + 2 ) || 'a_parent_object_id => ' || a_parent_id_param || ' ) ;' ) ;
 
 END ;
 $$ ;
