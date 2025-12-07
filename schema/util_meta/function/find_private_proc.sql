2e30b86b107ff643ae0c68ca451e83b12f5255d7
Gregory Siems <gsiems@gmail.com>
2025-12-06 23:13:29 -0600

    Fix parameter name in generated code

--- schema/util_meta/function/find_private_proc.sql
+++ schema/util_meta/function/find_private_proc.sql
@@ -21,41 +21,55 @@ DECLARE
     r record ;
     r2 record ;
     l_ret util_meta.ut_object ;
+    l_full_object_name text ;
+    --l_base_schema_name text ;
 
 BEGIN
 
+    l_full_object_name := a_proc_schema || '.' || a_proc_name ;
+    --l_base_schema_name := util_meta.base_name ( a_proc_schema ) ;
     FOR r IN (
-        SELECT schema_name
-            FROM util_meta.schemas
-            ORDER BY CASE
-                    WHEN schema_name IS NOT DISTINCT FROM a_proc_schema THEN 1
-                    WHEN schema_name ~ '^priv_' THEN 2
-                    WHEN schema_name ~ '^_' THEN 3
-                    WHEN schema_name ~ '_priv$' THEN 4
-                    --WHEN schema_name IS NOT DISTINCT FROM a_table_schema THEN 5
-                    ELSE 6
-                    END ) LOOP
+        WITH n AS (
+            SELECT schema_name,
+                    a_proc_name AS proc_name,
+                    l_full_object_name AS full_proc_name,
+                    util_meta.base_name ( schema_name ) AS base_schema_name,
+                    util_meta.base_order ( schema_name, util_meta.base_name ( schema_name ) ) AS base_order
+                FROM util_meta.schemas
+        )
+        SELECT schema_name,
+                base_schema_name,
+                base_order,
+                proc_name,
+                full_proc_name,
+                CASE WHEN schema_name = base_schema_name THEN 1 ELSE base_order END AS schema_order
+            FROM n
+            ORDER BY 6,
+                1 ) LOOP
 
         FOR r2 IN (
-            SELECT schema_name,
-                    object_name,
-                    full_object_name,
-                    base_object_type,
-                    object_type
-                FROM util_meta.objects
-                WHERE schema_name = r.schema_name
-                    AND r.schema_name || '.' || a_proc_name IS NOT DISTINCT FROM full_object_name
-                ORDER BY CASE
-                        WHEN object_name IS NOT DISTINCT FROM a_proc_name THEN 1
-                        WHEN object_name = 'priv_' || a_proc_name THEN 2
-                        WHEN object_name = '_' || a_proc_name THEN 3
-                        WHEN object_name = a_proc_name || '_priv$' THEN 4
-                        ELSE 6
-                        END ) LOOP
+            SELECT obj.schema_name,
+                    obj.object_name,
+                    obj.full_object_name,
+                    obj.base_object_type,
+                    obj.object_type--,
+                    --r.schema_name AS x,
+                    --r.base_schema_name,
+                    --r.proc_name,
+                    --r.full_proc_name,
+                    --r.schema_order,
+                    --util_meta.base_order ( obj.object_name, r.proc_name ) AS base_object_order
+                FROM util_meta.objects obj
+                WHERE obj.schema_name ~ r.base_schema_name
+                    AND obj.object_name ~ r.proc_name
+                    AND obj.full_object_name IS DISTINCT FROM r.full_proc_name
+                    AND obj.object_type IN ( 'function', 'procedure' )
+                ORDER BY r.schema_order,
+                    util_meta.base_order ( obj.object_name, r.proc_name ) ) LOOP
 
-            l_ret.schema := r2.schema_name ;
-            l_ret.name := r2.object_name ;
-            l_ret.full_name := r2.full_object_name ;
+            l_ret.schema_name := r2.schema_name ;
+            l_ret.object_name := r2.object_name ;
+            l_ret.full_object_name := r2.full_object_name ;
             l_ret.base_object_type := r2.object_type ;
             l_ret.object_type := r2.object_type ;
 
