512d6adb9bfb98a8e75b929d2e299b46486a4601
Gregory Siems <gsiems@gmail.com>
2025-12-17 14:54:55 -0600

    Add option for specifying a different target directory. Add additional scripts. Be more consistent in not overwriting pre-existing files

--- util/schema_tools/init_database_scripts.sh
+++ util/schema_tools/init_database_scripts.sh
@@ -9,30 +9,59 @@ init_database_scripts.sh
 
 SYNOPSIS
 
-    init_database_scripts.sh database_name
+    init_database_scripts.sh [-o target_directory] -d database_name
 
 DESCRIPTION
 
     Creates the initial scripts for creating a database.
 
+OPTIONS
+
+    -d database_name
+
+        The name of the database to create scripts for
+
+    -o target_directory
+
+        The name directory to create the scripts in (defaults to ../../schema)
+
+    -h
+
+        Displays this help
+
 EOT
     exit 0
 }
 
-################################################################################
-# Calling arguments and initialization
 cd "$(dirname "$0")" || exit 1
 
-targetDir=../../schema
-if [ ! -d "${targetDir}" ]; then
-    mkdir -p "${targetDir}"
-fi
+########################################################################
+# Read the calling args
+while getopts 'hd:o:' arg; do
+    case ${arg} in
+        d) dbName="${OPTARG}" ;;
+        h) usage=1 ;;
+        o) targetDir="${OPTARG}" ;;
+        *) usage=1 ;;
+    esac
+done
 
-dbName="${1}"
+if [[ -n ${usage} ]]; then
+    usage
+fi
 
 if [[ -z ${dbName} ]]; then
     echo "Please specify a name for the new database"
     usage
+fi
+
+if [[ -z ${targetDir} ]]; then
+    targetDir=../../schema
+fi
+
+########################################################################
+if [ ! -d "${targetDir}" ]; then
+    mkdir -p "${targetDir}" || exit 1
 fi
 
 preamble="
@@ -45,15 +74,15 @@ SET search_path = pg_catalog ;
 "
 
 ################################################################################
-fileName=001_drop_database
-psqlFile="${targetDir}"/"${fileName}".sql
 
-if [[ -f ${psqlFile} ]]; then
-    echo "${psqlFile} already exists. Cowardly refusing to overwrite it."
-    exit 1
-fi
+function init_drop_database() {
+    fileName=001_drop_database
+    psqlFile="${targetDir}"/"${fileName}".sql
 
-cat <<EOT >"${psqlFile}"
+    if [[ -f ${psqlFile} ]]; then
+        echo "${psqlFile} already exists. Cowardly refusing to overwrite it."
+    else
+        cat <<EOT >"${psqlFile}"
 /* *
 
 ## Drop database
@@ -63,9 +92,7 @@ cat <<EOT >"${psqlFile}"
 */
 
 \\c postgres
-
 ${preamble}
-
 \\unset ON_ERROR_STOP
 
 -- Since "REVOKE CONNECT ON DATABASE ${dbName} FROM ALL" isn't an option, so:
@@ -130,28 +157,26 @@ SELECT pg_terminate_backend ( pid )
 \set ON_ERROR_STOP
 
 DROP DATABASE IF EXISTS ${dbName} ;
-
 EOT
+    fi
+}
 
 ################################################################################
-fileName=002_drop_roles
-psqlFile="${targetDir}"/"${fileName}".sql
+function init_drop_roles() {
+    fileName=002_drop_roles
+    psqlFile="${targetDir}"/"${fileName}".sql
 
-if [[ -f ${psqlFile} ]]; then
-    echo "${psqlFile} already exists. Cowardly refusing to overwrite it."
-    exit 1
-fi
-
-cat <<EOT >"${psqlFile}"
+    if [[ -f ${psqlFile} ]]; then
+        echo "${psqlFile} already exists. Cowardly refusing to overwrite it."
+    else
+        cat <<EOT >"${psqlFile}"
 /* *
 ## Drop roles
 
 [${fileName}](${fileName}.sql)
 
 */
-
 ${preamble}
-
 \\unset ON_ERROR_STOP
 
 DROP ROLE IF EXISTS ${dbName}_developer ;
@@ -162,28 +187,26 @@ DROP USER IF EXISTS ${dbName}_updt ;
 DROP ROLE IF EXISTS ${dbName}_owner ;
 
 \\set ON_ERROR_STOP
-
 EOT
+    fi
+}
 
 ################################################################################
-fileName=003_create_roles
-psqlFile="${targetDir}"/"${fileName}".sql
+function init_create_roles() {
+    fileName=003_create_roles
+    psqlFile="${targetDir}"/"${fileName}".sql
 
-if [[ -f ${psqlFile} ]]; then
-    echo "${psqlFile} already exists. Cowardly refusing to overwrite it."
-    exit 1
-fi
-
-cat <<EOT >"${psqlFile}"
+    if [[ -f ${psqlFile} ]]; then
+        echo "${psqlFile} already exists. Cowardly refusing to overwrite it."
+    else
+        cat <<EOT >"${psqlFile}"
 /**
 ## Create roles
 
 [${fileName}](${fileName}.sql)
 
 */
-
 ${preamble}
-
 \\unset ON_ERROR_STOP
 
 CREATE ROLE ${dbName}_owner NOLOGIN
@@ -220,28 +243,26 @@ GRANT ${dbName}_read TO ${dbName}_developer ;
 GRANT ${dbName}_updt TO ${dbName}_developer ;
 
 \\set ON_ERROR_STOP
-
 EOT
+    fi
+}
 
 ################################################################################
-fileName=004_create_database
-psqlFile="${targetDir}"/"${fileName}".sql
+function init_create_database() {
+    fileName=004_create_database
+    psqlFile="${targetDir}"/"${fileName}".sql
 
-if [[ -f ${psqlFile} ]]; then
-    echo "${psqlFile} already exists. Cowardly refusing to overwrite it."
-    exit 1
-fi
-
-cat <<EOT >"${psqlFile}"
+    if [[ -f ${psqlFile} ]]; then
+        echo "${psqlFile} already exists. Cowardly refusing to overwrite it."
+    else
+        cat <<EOT >"${psqlFile}"
 /**
 ## Create the database
 
 [${fileName}](${fileName}.sql)
 
 */
-
 ${preamble}
-
 \\unset ON_ERROR_STOP
 
 CREATE DATABASE ${dbName}
@@ -265,19 +286,19 @@ CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog ;
 --CREATE EXTENSION IF NOT EXISTS postgis ;
 --CREATE EXTENSION IF NOT EXISTS fuzzystrmatch ;
 CREATE EXTENSION IF NOT EXISTS plpgsql_check ;
-
 EOT
+    fi
+}
 
 ################################################################################
-fileName=006_create_util_schemas
-psqlFile="${targetDir}"/"${fileName}".sql
+function init_util_schema() {
+    fileName=006_create_util_schemas
+    psqlFile="${targetDir}"/"${fileName}".sql
 
-if [[ -f ${psqlFile} ]]; then
-    echo "${psqlFile} already exists. Cowardly refusing to overwrite it."
-    exit 1
-fi
-
-cat <<EOT >"${psqlFile}"
+    if [[ -f ${psqlFile} ]]; then
+        echo "${psqlFile} already exists. Cowardly refusing to overwrite it."
+    else
+        cat <<EOT >"${psqlFile}"
 /**
 ## Create the utility schemas
 
@@ -285,24 +306,25 @@ cat <<EOT >"${psqlFile}"
 
 */
 
-${preamble}
-
 \connect ${dbName}
+${preamble}
 
 EOT
 
-ls "${targetDir}" | grep -P "^1.+sql" | awk '{print "\\i " $1}' >>${psqlFile}
+        ls "${targetDir}" | grep -P "^1.+sql" | awk '{print "\\i " $1}' >>"${psqlFile}"
+    fi
+}
 
 ################################################################################
-fileName=007_create_data_schemas
-psqlFile="${targetDir}"/"${fileName}".sql
+function init_create_data_schemas() {
 
-if [[ -f ${psqlFile} ]]; then
-    echo "${psqlFile} already exists. Cowardly refusing to overwrite it."
-    exit 1
-fi
+    fileName=007_create_data_schemas
+    psqlFile="${targetDir}"/"${fileName}".sql
 
-cat <<EOT >"${psqlFile}"
+    if [[ -f ${psqlFile} ]]; then
+        echo "${psqlFile} already exists. Cowardly refusing to overwrite it."
+    else
+        cat <<EOT >"${psqlFile}"
 /**
 ## Create the data schemas
 
@@ -310,22 +332,22 @@ cat <<EOT >"${psqlFile}"
 
 */
 
+\connect ${dbName}
 ${preamble}
 
-\connect ${dbName}
-
 EOT
+    fi
+}
 
 ################################################################################
-fileName=008_create_api_schemas
-psqlFile="${targetDir}"/"${fileName}".sql
+function init_create_api_schemas() {
+    fileName=008_create_api_schemas
+    psqlFile="${targetDir}"/"${fileName}".sql
 
-if [[ -f ${psqlFile} ]]; then
-    echo "${psqlFile} already exists. Cowardly refusing to overwrite it."
-    exit 1
-fi
-
-cat <<EOT >"${psqlFile}"
+    if [[ -f ${psqlFile} ]]; then
+        echo "${psqlFile} already exists. Cowardly refusing to overwrite it."
+    else
+        cat <<EOT >"${psqlFile}"
 /**
 ## Create the API schemas
 
@@ -333,8 +355,104 @@ cat <<EOT >"${psqlFile}"
 
 */
 
+\connect ${dbName}
 ${preamble}
 
+EOT
+    fi
+}
+
+################################################################################
+function init_ownership_and_permissions() {
+    fileName=501_ownership_and_permissions
+    psqlFile="${targetDir}"/"${fileName}".sql
+
+    if [[ -f ${psqlFile} ]]; then
+        echo "${psqlFile} already exists. Cowardly refusing to overwrite it."
+    else
+        cat <<EOT >"${psqlFile}"
+/**
+## Ownership and Permissions
+
+[${fileName}](${fileName}.sql)
+
+### Goal
+
+To document rules regarding how ownership/privileges could be set and provide
+code to enforce those rules.
+
+### Approach
+
+Rather than setting object ownership and permissions in each individual DDL
+file the setting of ownership and permissions could be consolidated into one
+location... here.
+
+### Rule Basics
+
+1. All database objects should be owned by the same owner. In the example the
+"example_db_owner" role should be the owner of all objects (In a real
+application it is hoped that the owner is not named "example_db_owner"). Also,
+in a real application, there may be exceptions to this rule and those
+exceptions should be documented-- for example, if the database integrates some
+third-party tools such as ESRI).
+
+2. The database objects owner should be a NOLOGIN role.
+
+3. The database objects owner should be consistent across the different
+environments (local, development, test, and production).
+
+4. Permissions to "private" objects should be minimized and should be
+consistently applied across the different environments.
+
+5. Permissions to API objects should be should be consistently applied across
+the different environments.
+
+6. Grants to the "public" role should be minimized.
+
+*/
+
 \connect ${dbName}
+${preamble}
 
 EOT
+    fi
+}
+
+################################################################################
+function init_role_passwords() {
+    fileName=601_role_passwords
+    psqlFile="${targetDir}"/"${fileName}".sql
+
+    if [[ -f ${psqlFile} ]]; then
+        echo "${psqlFile} already exists. Cowardly refusing to overwrite it."
+    else
+        cat <<EOT >"${psqlFile}"
+/**
+## Role Passwords
+
+[${fileName}](${fileName}.sql)
+
+If it is necessary to hard-code some role passwords, ensure that they are not
+hard-coded in any files that are version controlled as they then become visible
+to anyone who has access to the code repository. Examples of this would include
+credentials for foreign data wrappers to databases on different servers.
+
+Instead, consider using files that are not version controlled to set the
+passwords and set up some other mechanism for sharing them as needed.
+
+*/
+
+EOT
+    fi
+}
+
+################################################################################
+init_drop_database
+init_drop_roles
+init_create_roles
+init_create_database
+init_util_schema
+init_create_data_schemas
+init_create_api_schemas
+init_ownership_and_permissions
+init_role_passwords
