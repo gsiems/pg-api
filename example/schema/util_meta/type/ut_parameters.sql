a4b7883e5d74c6d4e260645cd09cd0c6379190fd
Gregory Siems <gsiems@gmail.com>
2025-12-24 17:09:58 -0600

    Replace copies of util schema directories w/sym-links

--- example/schema/util_meta/type/ut_parameters.sql
+++
@@ -1,8 +0,0 @@
-CREATE TYPE util_meta.ut_parameters AS (
-        names text[], -- The list of calling parameter names
-        directions text[], -- The list of the parameter calling directions (in, out, inout)
-        datatypes text[], -- The list of the parameter datatypes
-        defaults text[], -- The list of the parameter defaults
-        descriptions text[], -- The list of the parameter descriptions
-        args text[] -- The list of named parameters to use when calling a function or procedure ("a_arg => a_arg")
-    ) ;
--- example/schema/util_meta/type/ut_parent_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TYPE util_meta.ut_parent_table AS (
-        column_names text,
-        parent_schema text,
-        parent_name text,
-        parent_full_name text,
-        parent_noun text,
-        parent_column_names text
-    ) ;
